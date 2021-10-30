fe384afc42f09dbd84df748f279d0165c16ae8d4
namphung1502 <namphung1502@gmail.com>
2021-10-30 16:38:43 +0700

    change start scene

--- src/js/Game.js
+++ src/js/Game.js
@@ -53,7 +53,7 @@ class Game extends Application {
     setup() {
         this.startScene = new Scene();
         this.stage.addChild(this.startScene);
-        this.startScene.setVisible(false);
+        this.startScene.setVisible(true);
 
         this.backgroundStart = new SpriteObject(resources["image/start_background.jpg"].texture);
         this.backgroundStart.setScale(0.8, 0.8);
@@ -73,7 +73,7 @@ class Game extends Application {
         this.gameScene = new Scene();
         this.stage.addChild(this.gameScene);
 
-        this.gameScene.setVisible(true);
+        this.gameScene.setVisible(false);
 
         this.levelLoader = new LevelLoader();
         this.levelManager = new LevelManager();
